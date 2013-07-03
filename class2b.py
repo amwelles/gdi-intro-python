3ce53f1794d9da59dab7f7116d35beeac2c225e9
Autumn Welles <amwelles@gmail.com>
2013-07-02 21:00:06 -0400

    != makes more sense

--- class2b.py
+++ class2b.py
@@ -1,6 +1,6 @@
 user_input = raw_input('would you like to quit? (y/n) ')
 
-while user_input == 'n':
+while user_input != 'y':
 	user_input = raw_input('would you like to quit? (y/n) ')
 
 print 'quitters never win!'
