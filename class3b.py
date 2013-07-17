127a23572daa134f46e68d97992c1394bfdebafe
Autumn Welles <amwelles@gmail.com>
2013-07-16 20:19:32 -0400

    finishing up class 3, add gitignore

--- class3b.py
+++ class3b.py
@@ -1,15 +1,25 @@
 from helpers import generate_cleaned_lines
 
 def is_word_in_file(word, filename):
-    result = 'that word was not found'
-    for line in generate_cleaned_lines(filename):
-        # line will be a string of each line of the file in order
-        # Your code goes here. Do something with the word and line variables
-        if word in line:
-        	result = 'that word was found!'
-        	break
-    print result
+	i = 0
+	lines_found = []
+	for line in generate_cleaned_lines(filename):
+		i += 1
+		# line will be a string of each line of the file in order
+		# Your code goes here. Do something with the word and line variables
+		if word in line:
+			lines_found.append(i)
+			# result = 'that word was found on line {0}'.format(i)
 
-input_word = raw_input("Enter a word to search for: ")
-answer = is_word_in_file(input_word, 'pride.txt')
-# Display the answer in some meaningful way
+	if len(lines_found) > 0:
+		print 'your word was found on these lines:'
+		print lines_found
+		print 'total number of lines that word was found on: {0}'.format(len(lines_found))
+		
+	else:
+		print 'your word was not found :('
+
+# this code ensures that imports don't make this stuff below run
+if __name__ == '__main__':
+	input_word = raw_input("Enter a word to search for: ")
+	answer = is_word_in_file(input_word, 'pride.txt')
--- class4.py
+++ class4.py
@@ -1,1 +1,13 @@
-class4.py
+# set(name_of_list) makes list items unique
+# len() : length
+# range() : range of integers
+# sorted() : sorts a list
+# enumerate() : returns list of tuples from the list [(0, value), (1, value), (2, value)]
+# zip() : returns list of tuples with element from each iterable
+## useful for looping through multiple lists
+## for elem1, elem2 in zip(a, b):
+##     print 'eleme1 is {0} and elem2 is {1}'.format(elem1, elem2)
+
+# easier string formatting
+## print name, 'is in the', index, 'position' <-- adds spaces where commas are
+## message = '{0} is in the {1} position'.format(name, index)
